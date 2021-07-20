d87765443ebfa39d255e3703c6a36e06f2dceca9
damoshushu@126.com <zky823@126.com>
2021-07-21 00:27:21 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,53 +1,55 @@
 const HashMap = require('./HashMap')
 
+
+var isExpired = function (now, value) {
+  if (!value) return false;
+  return now > value.expireTime;
+}
+var checkExpire = function (that, key, value) {
+  if (isExpired(new Date().getTime(), value)) {
+    removeWithCallBack(that, key);
+    return null;
+  }
+  return value.value;
+}
+var removeWithCallBack = function (that, key) {
+  var item = that.cache.get(key) || {};
+  if (that.removeCallBack) {
+    that.removeCallBack(key, item.value)
+  }
+  return that.cache.remove(key);
+}
+var houseKeeping = function (that) {
+  let anyEviction = false;
+  var keys = that.cache.keySet();
+  if (keys.length < 1) return;
+  var now = new Date().getTime();
+  for (var i in keys) {
+    if (isExpired(now, that.cache.get(keys[i]))) {
+      removeWithCallBack(that, key)
+      anyEviction = true;
+    }
+  }
+  return anyEviction;
+}
+var ensurePut = function (that, key, value, duration) {
+  var keys = that.cache.keySet();
+  if (!that.cache.get(key) && keys.length >= that.size && !houseKeeping(that)) {
+    removeWithCallBack(that, keys[0]);
+  }
+  var now = new Date().getTime();
+  var expireTime = duration && duration >= 0 ? now + duration : now + that.globalDuration;
+  that.cache.put(key, {
+    value,
+    expireTime
+  });
+}
+
 function KSCache(size) {
   this.globalDuration = 24 * 60 * 60; // default duration 24 hours
   this.size = size;
   this.cache = new HashMap();
   this.removeCallBack = undefined;
-  isExpired = function (now, value) {
-    if (!value) return false;
-    return now > value.expireTime;
-  }
-  checkExpire = function (that, key, value) {
-    if (isExpired(new Date().getTime(), value)) {
-      removeWithCallBack(that, key);
-      return null;
-    }
-    return value.value;
-  }
-  removeWithCallBack = function (that, key) {
-    var item = that.cache.get(key) || {};
-    if (that.removeCallBack) {
-      that.removeCallBack(key, item.value)
-    }
-    return that.cache.remove(key);
-  }
-  houseKeeping = function (that) {
-    let anyEviction = false;
-    var keys = that.cache.keySet();
-    if (keys.length < 1) return;
-    var now = new Date().getTime();
-    for (var key in keys) {
-      if (isExpired(now, that.cache.get(key))) {
-        removeWithCallBack(that, key)
-        anyEviction = true;
-      }
-    }
-    return anyEviction;
-  }
-  ensurePut = function (that, key, value, duration) {
-    var keys = that.cache.keySet();
-    if (!that.cache.get(key) && keys.length >= that.size && !houseKeeping(that)) {
-      removeWithCallBack(that, keys[0]);
-    }
-    var now = new Date().getTime();
-    var expireTime = duration && duration >= 0 ? now + duration : now + that.globalDuration;
-    that.cache.put(key, {
-      value,
-      expireTime
-    });
-  }
 }
 KSCache.prototype = {
   setRemoveCallBack: function (callBack) {
